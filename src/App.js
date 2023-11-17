bc8111094b95d5e6adec3e77b554bbab62e11c69
Sergei Kuklin <sergei.kuklin@forto.com>
2023-11-17 10:24:27 +0100

    Add ui

--- src/App.js
+++ src/App.js
@@ -1,24 +1,227 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Select,
+  MenuItem,
+  Typography,
+  Container,
+  InputLabel,
+  FormControl,
+  Radio,
+  RadioGroup,
+  FormControlLabel,
+  FormLabel,
+  Grid,
+  Tooltip,
+  Chip,
+  Box,
+} from '@mui/material';
+import {
+  Timeline,
+  TimelineItem,
+  TimelineSeparator,
+  TimelineConnector,
+  TimelineContent,
+  TimelineDot,
+} from '@mui/lab';
+import { timelineItemClasses } from '@mui/lab/TimelineItem';
+import {
+  LocalShipping,
+  DirectionsBoat,
+  CheckCircleOutline,
+  Flag,
+  Share,
+} from '@mui/icons-material';
+import { Fragment, useState } from 'react';
+import { data } from './data';
 
 function App() {
+  const [trackableId, setTrackableId] = useState(
+    data.houseCargos[0].trackableId
+  );
+
+  const [view, setView] = useState('steps');
+
+  const houseCargo = data.houseCargos.find(
+    (houseCargo) => houseCargo.trackableId === trackableId
+  );
+
+  const renderView = () => {
+    if (view === 'steps') {
+      return houseCargo.steps.map((step) => {
+        const sharedWithHC = data.houseCargos
+          .filter((hc) => {
+            if (hc.trackableId === trackableId) return false;
+            return hc.steps.some((s) => s.stepId === step.stepId);
+          })
+          .map((hc) => hc.trackableId);
+
+        return (
+          <Fragment key={step.stepId}>
+            <TimelineItem>
+              <TimelineSeparator>
+                <TimelineDot color="primary" variant="outlined">
+                  <Tooltip title={`${step.type} - ${step.equipment}`}>
+                    {step.type === 'truck' ? (
+                      <LocalShipping />
+                    ) : (
+                      <DirectionsBoat />
+                    )}
+                  </Tooltip>
+                </TimelineDot>
+                <TimelineConnector />
+              </TimelineSeparator>
+              <TimelineContent>
+                <Box py={1} display="flex" alignItems="center" spacing={2}>
+                  <Box mr={2}>{step.partnerBooking.partnerName}</Box>
+                  {sharedWithHC.length > 0 ? (
+                    <Tooltip title={`Shared with ${sharedWithHC.join(', ')}`}>
+                      <Share />
+                    </Tooltip>
+                  ) : null}
+                </Box>
+              </TimelineContent>
+            </TimelineItem>
+
+            {step.events.map((event) => (
+              <TimelineItem key={event.code}>
+                <TimelineSeparator>
+                  <div style={{ width: '36px' }}>
+                    <div style={{ width: '15px', margin: '0 auto' }}>
+                      <TimelineDot />
+                    </div>
+                  </div>
+                  <TimelineConnector />
+                </TimelineSeparator>
+                <TimelineContent>
+                  <div>{event.code}</div>
+                  {event.date && (
+                    <Typography variant="secondary">
+                      {Intl.DateTimeFormat().format(new Date(event.date))}
+                    </Typography>
+                  )}
+                </TimelineContent>
+              </TimelineItem>
+            ))}
+          </Fragment>
+        );
+      });
+    }
+
+    return data.milestones.map((milestone, index) => {
+      return (
+        <Fragment key={index}>
+          <TimelineItem>
+            <TimelineSeparator>
+              <TimelineDot color="primary" variant="outlined">
+                <Flag />
+              </TimelineDot>
+              <TimelineConnector />
+            </TimelineSeparator>
+            <TimelineContent>{milestone.name}</TimelineContent>
+          </TimelineItem>
+
+          {milestone.events.map((mEvent) => {
+            const step = houseCargo.steps.find(
+              (step) => step.stepId === mEvent.stepId
+            );
+
+            if (!step) return null;
+
+            const event = step.events.find(
+              (event) => event.code === mEvent.eventCode
+            );
+
+            return (
+              <TimelineItem key={event.code}>
+                <TimelineSeparator>
+                  <div style={{ width: '36px' }}>
+                    <div style={{ width: '15px', margin: '0 auto' }}>
+                      <TimelineDot />
+                    </div>
+                  </div>
+                  <TimelineConnector />
+                </TimelineSeparator>
+                <TimelineContent>
+                  <div>{event.code}</div>
+                  {event.date && (
+                    <Typography variant="secondary">
+                      {Intl.DateTimeFormat().format(new Date(event.date))}
+                    </Typography>
+                  )}
+                </TimelineContent>
+              </TimelineItem>
+            );
+          })}
+        </Fragment>
+      );
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="sm">
+      <Typography fontWeight={700} variant="h5" py={2}>
+        Shipment {data.shipmentId} - {data.customer}
+      </Typography>
+
+      <Grid container spacing={4}>
+        <Grid item>
+          <InputLabel>House Cargo</InputLabel>
+          <FormControl variant="standard" sx={{ minWidth: 120 }}>
+            <Select
+              value={trackableId}
+              label="House Cargo"
+              onChange={(event) => setTrackableId(event.target.value)}
+            >
+              {data.houseCargos.map((hc) => (
+                <MenuItem key={hc.trackableId} value={hc.trackableId}>
+                  {hc.trackableId}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </Grid>
+
+        <Grid item>
+          <FormControl>
+            <FormLabel>View</FormLabel>
+            <RadioGroup
+              row
+              name="view"
+              value={view}
+              onChange={(event) => setView(event.target.value)}
+            >
+              <FormControlLabel
+                value="steps"
+                control={<Radio />}
+                label="Steps"
+              />
+              <FormControlLabel
+                value="milestones"
+                control={<Radio />}
+                label="Milestones"
+              />
+            </RadioGroup>
+          </FormControl>
+        </Grid>
+      </Grid>
+      <Timeline
+        sx={{
+          [`& .${timelineItemClasses.root}:before`]: {
+            flex: 0,
+            padding: 0,
+          },
+        }}
+      >
+        {renderView()}
+
+        <TimelineItem>
+          <TimelineSeparator>
+            <TimelineDot color="primary" variant="outlined">
+              <CheckCircleOutline />
+            </TimelineDot>
+          </TimelineSeparator>
+        </TimelineItem>
+      </Timeline>
+    </Container>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,160 @@
+export const data = {
+  customer: 'Disney',
+  shipmentId: 'S123',
+  houseCargos: [
+    {
+      trackableId: '12345',
+      steps: [
+        {
+          stepId: '1',
+          type: 'truck',
+          partnerBooking: {
+            partnerName: 'some trucker',
+          },
+          events: [
+            {
+              code: 'gateIn',
+              type: 'actual',
+              date: '2023-11-14T18:25:43.511Z',
+              milestone: 'POL',
+            },
+          ],
+          equipment: 'container 1',
+        },
+        {
+          stepId: '2',
+          type: 'vessel',
+          partnerBooking: {
+            partnerName: 'HapagLloyd',
+          },
+          events: [
+            {
+              code: 'vesselDeparture',
+              type: 'estimated',
+              date: '2023-11-17T18:25:43.511Z',
+              milestone: 'POL',
+            },
+            {
+              code: 'vesselArrival',
+              type: 'estimated',
+              date: '2023-11-27T18:25:43.511Z',
+              milestone: 'POD',
+            },
+          ],
+          equipment: 'container 1',
+        },
+        {
+          stepId: '3',
+          type: 'truck',
+          partnerBooking: {
+            partnerName: 'other trucker',
+          },
+          events: [
+            {
+              code: 'gateOut',
+              type: 'unknown',
+              date: null,
+              milestone: 'POD',
+            },
+          ],
+          equipment: 'container 1',
+        },
+      ],
+    },
+    {
+      trackableId: '34567',
+      steps: [
+        {
+          stepId: '4',
+          type: 'truck',
+          partnerBooking: {
+            partnerName: 'the trucker',
+          },
+          events: [
+            {
+              code: 'gateIn',
+              type: 'actual',
+              date: '2023-11-12T18:25:43.511Z',
+              milestone: 'POL',
+            },
+          ],
+          equipment: 'container 2',
+        },
+        {
+          stepId: '2',
+          type: 'vessel',
+          partnerBooking: {
+            partnerName: 'HapagLloyd',
+          },
+          events: [
+            {
+              code: 'vesselDeparture',
+              type: 'estimated',
+              date: '2023-11-17T18:25:43.511Z',
+              milestone: 'POL',
+            },
+            {
+              code: 'vesselArrival',
+              type: 'estimated',
+              date: '2023-11-27T18:25:43.511Z',
+              milestone: 'POD',
+            },
+          ],
+          equipment: 'container 2',
+        },
+        {
+          stepId: '5',
+          type: 'barge',
+          partnerBooking: {
+            partnerName: 'captain Jack Sparrow',
+          },
+          events: [
+            {
+              code: 'gateOut',
+              type: 'unknown',
+              date: null,
+              milestone: 'POD',
+            },
+          ],
+          equipment: 'container 2',
+        },
+      ],
+    },
+  ],
+  milestones: [
+    {
+      name: 'POL',
+      events: [
+        {
+          stepId: '1',
+          eventCode: 'gateIn',
+        },
+        {
+          stepId: '2',
+          eventCode: 'vesselDeparture',
+        },
+        {
+          stepId: '4',
+          eventCode: 'gateIn',
+        },
+      ],
+    },
+    {
+      name: 'POD',
+      events: [
+        {
+          stepId: '3',
+          eventCode: 'gateOut',
+        },
+        {
+          stepId: '2',
+          eventCode: 'vesselArrival',
+        },
+        {
+          stepId: '5',
+          eventCode: 'gateOut',
+        },
+      ],
+    },
+  ],
+};
